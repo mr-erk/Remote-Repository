8c90d82d6444e8d931eff6e525f5bf725dd4c3e4
Erk <erk.0805112002@gmail.com>
2025-10-24 23:55:32 +0300

    anither change of dev2_file

--- dev2_file.py
+++ dev2_file.py
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-print('Feature from person2')
-print('Extra line from person2')
-print('Feachure from person2, but its dev3')
-=======
-print('Feachure from person2, but it's dev3')
->>>>>>> dev3
+print('Feachure from person2, but it's main')
